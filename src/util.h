53702ab184e9133a9cfe28406c71cfdabf2e4db5
Vito Caputo <vcaputo@gnugeneration.com>
2017-03-24 01:51:50 -0700

    util: switch VWM_BUG macro from including strerror

--- src/util.h
+++ src/util.h
@@ -6,7 +6,7 @@
 
 #define VWM_ERROR(_fmt, _args...)	fprintf(stderr, "%s:%i\t%s() "_fmt"\n", __FILE__, __LINE__, __FUNCTION__, ##_args)
 #define VWM_PERROR(_fmt, _args...)	fprintf(stderr, "%s:%i\t%s() "_fmt"; %s\n", __FILE__, __LINE__, __FUNCTION__, ##_args, strerror(errno))
-#define VWM_BUG(_fmt, _args...)		fprintf(stderr, "BUG %s:%i\t%s() "_fmt"; %s\n", __FILE__, __LINE__, __FUNCTION__, ##_args, strerror(errno))
+#define VWM_BUG(_fmt, _args...)		VWM_ERROR("BUG: "_fmt, ##_args)
 
 #ifdef TRACE
 #define VWM_TRACE(_fmt, _args...)	fprintf(stderr, "%s:%i\t%s() "_fmt"\n", __FILE__, __LINE__, __FUNCTION__, ##_args)
