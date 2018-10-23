b6a393bc55476bb65a974e8bd81482287b7f72f3
Vito Caputo <vcaputo@pengaru.com>
2018-10-23 01:06:04 -0700

    *: update copyright lines to include 2018

--- src/xwindow.c
+++ src/xwindow.c
@@ -1,7 +1,7 @@
 /*
  *                                  \/\/\
  *
- *  Copyright (C) 2012-2017  Vito Caputo - <vcaputo@pengaru.com>
+ *  Copyright (C) 2012-2018  Vito Caputo - <vcaputo@pengaru.com>
  *
  *  This program is free software: you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 3 as published
