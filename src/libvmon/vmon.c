0ba17d3da478f0364f3503aa98b95ea778ba570b
Vito Caputo <vcaputo@pengaru.com>
2024-07-19 18:51:28 -0700

    *: switch to gplv2
    Part of the reason for adding headless support in vmon is to
    facilitate embedded use cases.  These are often incompatible with
    anti-tivoization aspects of gplv3.
    
    I am the copyright holder of all this stuff so it's entirely fine
    to switch to gplv2.  Phil Freeman contributed one trivial patch
    (4183fbd), regardless I checked if he had any objections to the
    gplv2 switch and he had none.
    
    So here we go, gplv2 all the things.

--- src/libvmon/vmon.c
+++ src/libvmon/vmon.c
@@ -8,7 +8,7 @@
  *  Copyright (c) 2012-2017  Vito Caputo - <vcaputo@pengaru.com>
  *
  *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3 as
+ *  it under the terms of the GNU General Public License version 2 as
  *  published by the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
