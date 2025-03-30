451593bfc3f891b532f91f60cc74814674b74050
Vito Caputo <vcaputo@pengaru.com>
2025-03-30 13:44:18 -0700

    vmon: stub out -T/--no-threads argument
    This will suppress showing threads in the output, and save on
    much of the overhead associated with monitoring them.
    
    Subsequent commits will augment libvmon to support this use case,
    followed by charts changes to support a threads-hidden mode which
    must still consider any of the threads children.

--- src/vmon.c
+++ src/vmon.c
@@ -50,6 +50,7 @@ typedef struct vmon_t {
 	int		dump_procs;
 	int		mem_locked;
 	int		reaper;
+	int		no_threads;
 	time_t		start_time;
 	int		snapshots_interval;
 	int		snapshot;
@@ -207,6 +208,7 @@ static void print_help(void)
 		" -D  --dump-procs  Dump libvmon internal processes table (debugging aid)\n"
 		" -L  --mem-locked  Lock in memory using mlockall(MCL_CURRENT|MCL_FUTURE)\n"
 		" -R  --reaper      Become the child subreaper (see prctl(2); PR_SET_CHILD_SUBREAPER)\n"
+		" -T  --no-threads  Don't monitor threads beyond tracking their potential children\n"
 		" -W  --width       Chart width\n"
 		" -z  --hertz       Sample rate in hertz\n"
 		"-------------------------------------------------------------------------------"
@@ -502,6 +504,9 @@ static int vmon_handle_argv(vmon_t *vmon, int argc, const char * const *argv)
 			last = argv;
 		} else if (is_flag(*argv, "-R", "--reaper")) {
 			vmon->reaper = 1;
+			last = argv;
+		} else if (is_flag(*argv, "-T", "--no-threads")) {
+			vmon->no_threads = 1;
 			last = argv;
 		} else if ((*argv)[0] == '-') {
 			VWM_ERROR("Unrecognized argument: \"%s\", try --help\n", argv[0]);
