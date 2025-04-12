82c0242318c17b264f1ed1ddc982eb5047568545
Vito Caputo <vcaputo@pengaru.com>
2025-04-11 21:46:45 -0700

    *: update some copyright dates for files being touched

--- src/charts.c
+++ src/charts.c
@@ -1,7 +1,7 @@
 /*
  *                                  \/\/\
  *
- *  Copyright (C) 2012-2024  Vito Caputo - <vcaputo@pengaru.com>
+ *  Copyright (C) 2012-2025  Vito Caputo - <vcaputo@pengaru.com>
  *
  *  This program is free software: you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
